d1f2137ab63ccf15dfab9d14383eac72a2a875f1
David Kun <davidkun4@gmail.com>
2013-09-24 00:01:56 -0400

    checking commit and add

---
+++ agent.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+class agent():
+  """ Represent an agent in
+  a multi-vehicle system (MAS).
+  
+  The agent is dynamic, reads location transmissions,
+  and has the ability to communicate its own state."""
+
+a = agent()
+a.hour = 11
+a.minute = 2
+
+print a.minute
