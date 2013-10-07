6ea1187bc431b10b90e2ab3cad26cd3a1405c93c
David Kun <davidkun4@gmail.com>
2013-10-06 23:50:31 -0400

    added testing file and worked on agent.py

--- agent.py
+++ agent.py
@@ -1,9 +1,31 @@
 #!/usr/bin/env python
+import numpy as np
 
-class agent():
-  """ Represent an agent in
-  a multi-vehicle system (MAS).
+
+class point():
+  """ Represents a UAV in a multi-vehicle system (MAS).
   
-  The agent is dynamic, reads location transmissions,
-  and has the ability to communicate its own state."""
-
+  The agent is dynamic, it reads location transmissions,
+  and has the ability to communicate its own state.
+  """
+  
+  def __init__( self, X=np.zeros((4,1)), Ts=1.0 ):
+    
+    self.X = X
+    self.Xhist = np.array([[self.X[0,0],self.X[1,0]]])
+    
+    self.A = np.matrix([ [1,0,Ts,0],
+                         [0,1,0,Ts],
+                         [0,0,0,0],
+                         [0,0,0,0] ])
+  
+    self.B = np.matrix([ [0,0],
+                         [0,0],
+                         [1,0],
+                         [0,1] ])
+  
+  def f(self, u=np.zeros((2,1))):
+    self.X = self.A*self.X + self.B*u
+    L = np.shape(self.Xhist)[0]
+    x,y = self.X[0,0], self.X[1,0]
+    self.Xhist = np.insert(self.Xhist, L, [x,y], axis=0)
---
+++ test.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import matplotlib.pyplot as plt
+import numpy as np
+import time, random
+import agent
+
+
+def main():
+  """
+  This is a testing function to make sure
+  the code is running properly.
+  
+  Run this to check for errors.
+  """
+  
+  point1 = agent.point(np.matrix([[1.0],[1.0],[0.2],[0.3]]))
+  
+  runSim(point1)
+
+
+
+
+def runSim(vid):
+  """
+  Simulate vehicle movement in the xy-plane.
+  """
+  fig = plt.figure()
+  ax = fig.add_subplot(111, \
+  autoscale_on=False, \
+  xlim=(0, 20), ylim=(0, 20))
+  plt.ion()
+  plt.hold(True)
+  abc = ax.plot(vid.X[0,0], vid.X[1,0], 'o')
+  
+  for i in range(10):
+    plt.setp(abc[0], data=(vid.X[0,0],vid.X[1,0]))
+    plt.draw()
+    #time.sleep(1)
+    vid.f(u=np.random.rand(2,1))
+  
+  plt.ioff()
+  track = ax.plot(vid.Xhist[:,0],vid.Xhist[:,1])
+  plt.setp(track, linestyle=':', color='b')
+  plt.show()
+
+
+if __name__ == '__main__':
+  main()
+
