8d99dc6e9be237fcfcf0e7669403d7672cb28126
David Kun <davidkun4@gmail.com>
2013-10-16 23:09:42 -0400

    controller design added

--- agent.py
+++ agent.py
@@ -1,31 +1,60 @@
 #!/usr/bin/env python
 import numpy as np
-
+from simParams import *
 
 class point():
-  """ Represents a UAV in a multi-vehicle system (MAS).
+  """ 
+  'point' represents a point-mass agent in a multi-vehicle system (MAS).
   
   The agent is dynamic, it reads location transmissions,
-  and has the ability to communicate its own state.
+  and has the ability to communicate its own state, X, where:
+  
+  X = [ x ]
+      | y |
+      | x'|
+      [ y']
   """
   
-  def __init__( self, X=np.zeros((4,1)), Ts=1.0 ):
+  def __init__( self, X=np.zeros((4,1)) ):
+    """ 
+    Initialize point dynamics 
+    """
     
     self.X = X
     self.Xhist = np.array([[self.X[0,0],self.X[1,0]]])
     
-    self.A = np.matrix([ [1,0,Ts,0],
-                         [0,1,0,Ts],
-                         [0,0,0,0],
-                         [0,0,0,0] ])
+    self.A = np.matrix([ [1,0,Ts,0 ],
+                         [0,1,0 ,Ts],
+                         [0,0,1 ,0 ],
+                         [0,0,0 ,1 ] ])
   
-    self.B = np.matrix([ [0,0],
-                         [0,0],
-                         [1,0],
-                         [0,1] ])
-  
-  def f(self, u=np.zeros((2,1))):
+    self.B = np.matrix([ [0      , 0        ],
+                         [0      , 0        ],
+                         [Ts**2/2, 0        ],
+                         [0      , Ts**2/2] ])
+
+    self.C = np.asmatrix(np.identity(4))
+    
+    self.Z = np.zeros([4,1])
+    
+    
+  def step( self, u=np.zeros((2,1)) ):
+    """
+    Step in time (integrate)
+    """
+    
+    # State
     self.X = self.A*self.X + self.B*u
-    L = np.shape(self.Xhist)[0]
-    x,y = self.X[0,0], self.X[1,0]
-    self.Xhist = np.insert(self.Xhist, L, [x,y], axis=0)
+
+    # Integral Control
+    self.XI = self.Z - np.r_[wPts[target].T,np.zeros([2,1])]
+    
+    # Measurable Output
+    self.Z = self.C*self.X
+    
+    
+    # Update History of X Position (for plotting)
+    sizeX = np.shape(self.Xhist)[0]
+    xPos,yPos = self.X[0,0], self.X[1,0]
+    self.Xhist = np.insert(self.Xhist, sizeX, [xPos,yPos], axis=0)
+  
--- test.py
+++ test.py
@@ -2,8 +2,10 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-import time, random
+import time, sys
+from   scipy import linalg
 import agent
+from   simParams import *
 
 
 def main():
@@ -14,36 +16,77 @@ def main():
   Run this to check for errors.
   """
   
-  point1 = agent.point(np.matrix([[1.0],[1.0],[0.2],[0.3]]))
+  # Initial States
+  IC1 = np.matrix([ [1],
+                    [1],
+                    [0],
+                    [0] ])
   
-  runSim(point1)
+  
+  # Create Agent
+  p1 = agent.point(IC1)
 
+  # Controllability Grammian
+  p1.Wc = gram(p1.A, p1.B, int(T/Ts))
 
-
-
-def runSim(vid):
+  # Begin Simulation
+  runSim(p1)
+  
+  
+def runSim(leader):
   """
-  Simulate vehicle movement in the xy-plane.
+  Simulate vehicle movement in the xy-plane. 
   """
+  
   fig = plt.figure()
   ax = fig.add_subplot(111, \
-  autoscale_on=False, \
-  xlim=(0, 20), ylim=(0, 20))
+        autoscale_on=False, \
+        xlim=(0, 10), ylim=(0, 10))
   plt.ion()
   plt.hold(True)
-  abc = ax.plot(vid.X[0,0], vid.X[1,0], 'o')
+  abc = ax.plot(leader.X[0,0], leader.X[1,0], 'o')
   
-  for i in range(10):
-    plt.setp(abc[0], data=(vid.X[0,0],vid.X[1,0]))
+  N = int(T/Ts)
+  print N
+  for k in range(N):
+    plt.setp(abc[0], data=(leader.X[0,0],leader.X[1,0]))
     plt.draw()
-    #time.sleep(1)
-    vid.f(u=np.random.rand(2,1))
+    time.sleep(.2)
+    
+    # Controller Design
+    u = leader.B.T * leader.A.T**(N-1-k) * \
+        linalg.inv(leader.Wc) * np.r_[wPts[target].T,np.zeros([2,1])]
+    
+    leader.step(u)
   
   plt.ioff()
-  track = ax.plot(vid.Xhist[:,0],vid.Xhist[:,1])
+  
+  # Plot Vehicle Path
+  track = ax.plot(leader.Xhist[0:-1,0],leader.Xhist[0:-1,1])
   plt.setp(track, linestyle=':', color='b')
+  
+  # Show Plot
   plt.show()
 
+
+
+def gram(A, B, N):
+  """ 
+  Wc = gram(A, B, N) computes the discrete time 
+  Controllability Grammian of the system.
+  """
+  
+  import numpy as np
+  from scipy import linalg
+  
+  Wc = np.zeros(np.shape(A))
+  
+  for k in range(N):
+    Wc = Wc + A**(N-1-k) * \
+         B * B.T * \
+         (A.T)**(N-1-k)
+  
+  return Wc
 
 if __name__ == '__main__':
   main()
